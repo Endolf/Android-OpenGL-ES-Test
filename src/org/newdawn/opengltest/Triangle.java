455a4f637956d37e47513b04abd26d60ced35a62
Endolf <endolf@computerbooth.com>
2013-04-15 13:27:32 +0100

    format the coords properly

--- src/org/newdawn/opengltest/Triangle.java
+++ src/org/newdawn/opengltest/Triangle.java
@@ -2,8 +2,8 @@ package org.newdawn.opengltest;
 
 public class Triangle extends Mesh {
 	public Triangle() {
-		super(new float[] {0.0f,  0.622008459f, 0.0f,-0.5f,
-				-0.311004243f, 0.0f,
+		super(new float[] {0.0f,  0.622008459f, 0.0f,
+				-0.5f, -0.311004243f, 0.0f,
 				0.5f, -0.311004243f, 0.0f}, 
 			new float[] {0.639f,0.769f,0.224f,1}, 
 			new short[] {0,1,2});
