14929ee65ae11b9961974eb1a3044fccdb98fb2c
Endolf <endolf@computerbooth.com>
2013-04-15 11:27:59 +0100

    Make the triangle android green

--- src/org/newdawn/opengltest/Triangle.java
+++ src/org/newdawn/opengltest/Triangle.java
@@ -5,7 +5,7 @@ public class Triangle extends Mesh {
 		super(new float[] {0.0f,  0.622008459f, 0.0f,-0.5f,
 				-0.311004243f, 0.0f,
 				0.5f, -0.311004243f, 0.0f}, 
-			new float[] {1,1,0,1}, 
+			new float[] {0.639f,0.769f,0.224f,1}, 
 			new short[] {0,1,2});
 	}
 }
