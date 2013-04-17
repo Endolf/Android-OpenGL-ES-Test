f678c8fd864de3792af2c874e5bbcf07fda91a1b
Endolf <endolf@computerbooth.com>
2013-04-17 12:04:36 +0100

    Pause and resume seem to cause double drawing some times.

--- src/org/newdawn/opengltest/OpenGLES20Activity.java
+++ src/org/newdawn/opengltest/OpenGLES20Activity.java
@@ -17,22 +17,22 @@ public class OpenGLES20Activity extends Activity {
         setContentView(mGLView);
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        // The following call pauses the rendering thread.
-        // If your OpenGL application is memory intensive,
-        // you should consider de-allocating objects that
-        // consume significant memory here.
-        mGLView.onPause();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        // The following call resumes a paused rendering thread.
-        // If you de-allocated graphic objects for onPause()
-        // this is a good place to re-allocate them.
-        mGLView.onResume();
-    }
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        // The following call pauses the rendering thread.
+//        // If your OpenGL application is memory intensive,
+//        // you should consider de-allocating objects that
+//        // consume significant memory here.
+//        mGLView.onPause();
+//    }
+//
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        // The following call resumes a paused rendering thread.
+//        // If you de-allocated graphic objects for onPause()
+//        // this is a good place to re-allocate them.
+//        mGLView.onResume();
+//    }
 }
